ca3152e336ab31e2c0aba0ce12171885e9bbb2aa
Ian Walston <ikwalston@comcast.net>
2018-11-26 21:54:17 -0500

    moved some files around, renamed GenomeData.rb to analyze.rb, added sample raw 23andme input and sample output. Rewrote README.md

--- GenomeData.rb
+++ GenomeData.rb
@@ -1,13 +1,16 @@
-#load a file from the raw genome data provided from 23andme
-genome_file = 'iangenome.txt'
+#Loads raw genome data provided from 23andme
+genome_file = 'raw23andme.txt'
 str = File.read(genome_file)
 puts "Loading " + genome_file
 
-#This code arranges the raw data into an array of 4 member arrays ordered {rsid, chromosome, position, genotype}
+#Remove comments from raw genome data
+str = str.gsub(/(^#.+)/, '')
+
+#Arranges the raw data into an array of 4 member arrays like this: {rsid, chromosome, position, genotype}
 split_str = str.split(" ")
 my_genome_array = []
-i = 0
 subarr = []
+i = 0
 split_str.each {|ele|
   i+=1
   subarr << ele
@@ -15,24 +18,21 @@ split_str.each {|ele|
     i=0
     my_genome_array << subarr
     subarr = []
-  end
-}
+  end}
 
-# This method counts the number of SNPs for each chromosome 
+#This method counts the number of SNPs for each chromosome 
 def chromosome_count(genome_array)
-
   ccount = Hash.new(0)
-
   genome_array.each {|entry| ccount[entry[1]] += 1}
   ccount.each {|chromosome, count| puts "Chromosome " + chromosome.to_s + " has " + count.to_s + " SNP data points" }
   total = 0
   ccount.values.each {|count| total+= count }
-  puts ""
-  puts "This data set contains a total of " + total.to_s + " data points"
+  puts "----"
+  puts total.to_s + " total data points"
   return ccount
 end
 
-# This method counts the number of heterozygous, homozygous, haploid, or missing SNPs
+#This method counts the number of heterozygous, homozygous, haploid, or missing SNPs
 def genotype_count(genome_array)
 
   gcount = Hash.new(0)
@@ -42,33 +42,26 @@ def genotype_count(genome_array)
   homozygous_genotypes_array = []
   heterozygous_genotypes_array = []
   nucleotides.each_char {|ele1| 
-  nucleotides.each_char {|ele2|	
-  homozygous_genotypes_array<<(ele1 + ele2) if ele1 == ele2	
-  heterozygous_genotypes_array<<(ele1 + ele2) if ele1 != ele2}}
-
+    nucleotides.each_char {|ele2|	
+      homozygous_genotypes_array<<(ele1 + ele2) if ele1 == ele2	
+        heterozygous_genotypes_array<<(ele1 + ele2) if ele1 != ele2}}
+  
+  #This adds up the number of different genotypes in the genome
   genome_array.each {|entry|
     if heterozygous_genotypes_array.include?(entry[3]) 
       gcount["Heterozygous"] += 1 
     elsif homozygous_genotypes_array.include?(entry[3])
-	  gcount["Homozygous"] += 1
+	    gcount["Homozygous"] += 1
     elsif nucleotides.include?(entry[3])
       gcount["Haploid"] += 1  
     else
       gcount["Missing"] += 1 
     end
   }
-  gcount.each {|genotype, count| puts "There are " + count.to_s + " " + genotype + " data points"}
+  gcount.each {|genotype, count| puts count.to_s + " " + genotype + " data points"}
 end
 
+#Method calls
 chromosome_count(my_genome_array)
-puts "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+puts "-----"
 genotype_count(my_genome_array)
-
-
-
-# I can't remember what I was doing with this hash. This is why it's so important to comment on your code, even if you're the only one who will ever read it.
-# rs_hash = {}
-# my_genome_array.each {|entry|
-#   rs_hash[entry[0]] = entry[3]
-#   }
-# print rs_hash
--- README.md
+++ README.md
@@ -1,13 +1,46 @@
 #Genome Project
 
-The input for this program is the raw genomic data provided by 23anMe. This data provides information on over 600,000 possible variations (SNPs) in human DNA. These SNPs are associated with the physical and mental differences between humans. 
+###Provides basic analysis of raw 23andMe Data.
 
+The input for this program is the raw genomic data provided by 23andMe. This data provides information on over 600,000 possible variations (SNPs) in human DNA. These SNPs are partially responsible for the physical and mental differences between humans. 
 
-genome.txt - This file includes my raw genomic data, exactly as it is provided by 23andMe. 
+GenomeData.rb - This file takes in genomes/raw23andme.txt as an input and converts it to a 2D array. GenomeData.rb contains two methods:
 
-iangenome.txt - This file is my raw genomic data with the description at the top cut off
+chromosome_count() : This method counts the number of SNPs located in each of the different human chromosomes
+genotype_count() : This method counts the number of each genotype in the genomic data that is put into the program. 
 
-GenomeData.rb - This file takes in iangenome.txt as an input and converts it to a 2D array. GenomeData.rb contains two methods:
+Here is a sample output from GenomeData.rb:
 
-    chromosome_count() : This method counts the number of SNPs located in each of the different human chromosomes
-    genotype_count() : This method counts the number of each genotype (hetero, homo, haploid, or if the genotype is missing) of each SNP in the genomic data that is put into the program. 
+Loading genomes/raw23andme.txt
+Chromosome 1 has 46662 SNP data points
+Chromosome 2 has 46128 SNP data points
+Chromosome 3 has 38517 SNP data points
+Chromosome 4 has 33915 SNP data points
+Chromosome 5 has 34387 SNP data points
+Chromosome 6 has 40384 SNP data points
+Chromosome 7 has 33053 SNP data points
+Chromosome 8 has 30268 SNP data points
+Chromosome 9 has 26586 SNP data points
+Chromosome 10 has 29210 SNP data points
+Chromosome 11 has 29328 SNP data points
+Chromosome 12 has 28451 SNP data points
+Chromosome 13 has 21679 SNP data points
+Chromosome 14 has 18696 SNP data points
+Chromosome 15 has 18281 SNP data points
+Chromosome 16 has 19199 SNP data points
+Chromosome 17 has 18743 SNP data points
+Chromosome 18 has 16490 SNP data points
+Chromosome 19 has 13002 SNP data points
+Chromosome 20 has 14494 SNP data points
+Chromosome 21 has 8461 SNP data points
+Chromosome 22 has 9098 SNP data points
+Chromosome X has 19478 SNP data points
+Chromosome Y has 2302 SNP data points
+Chromosome MT has 5075 SNP data points
+----
+601887 total data points
+-----
+404191 Homozygous data points
+163293 Heterozygous data points
+10212 Missing data points
+24191 Haploid data points
